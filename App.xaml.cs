7fa0f24e39dbe4e94948357b8082000d547a6cc0
Sy <Sy@DESKTOP-11DG6QT>
2025-10-15 12:34:36 +0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace IT008.Q13_Project___fromScratch
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
