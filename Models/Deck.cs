200df2f8eff95109a1214f4cf643d5e21942a42b
mjvorfreudej <24521498@gm.uit.edu.vn>
2025-10-21 16:11:18 +0700

    add deck.cs

--- Models/Deck.cs
+++ Models/Deck.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace IT008.Q13_Project___fromScratch.Models
 {
-    internal class Deck
+    class Deck
     {
+        public int ID { get; private set; }
+        public string Name { get; set; } = " ";
+        public string Description { get; set; } = " ";
+        public List<Card> Cards { get; set; } = new List<Card>();
     }
 }
