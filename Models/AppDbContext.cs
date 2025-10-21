d5ec7ee1f46e24e61d2eb305fc994a0db511e205
Nguyen Thi Ngoc Huyen <hinduck3206@gmail.com>
2025-10-21 16:47:22 +0700

    add DeckTable in AppDbContext.cs

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Microsoft.EntityFrameworkCore;
 namespace IT008.Q13_Project___fromScratch.Models
 {
-    internal class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public DbSet<Deck> Decks { get; set; }
+        public DbSet<Card> Cards { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Deck>(e =>
+            {
+                e.ToTable("Decks");
+                e.HasKey(d => d.ID);
+                e.Property(d => d.Name).IsRequired().HasMaxLength(200);
+                e.Property(d => d.Description).HasMaxLength(1000);
+            }
+            );
+        }
     }
 }
+      
