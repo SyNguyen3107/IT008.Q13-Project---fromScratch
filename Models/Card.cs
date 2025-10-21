56221388ac181167509c8dfe654216c622aae168
Nguyen Thi Ngoc Huyen <hinduck3206@gmail.com>
2025-10-21 16:15:06 +0700

    add card.cs

--- Models/Card.cs
+++ Models/Card.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace IT008.Q13_Project___fromScratch.Models
 {
-    internal class Card
+    public class Card
     {
+        public int ID { get; private set; }
+        public int DeckId { get; set; }
+
+        //Nội dung mặt trước
+        public string FrontText { get; set; }
+        public string FrontImagePath { get; set; }
+        public string FrontAudioPath { get; set; }
+         
+        //Nội dung mặt sau
+
+        public string BackText { get; set; }
+        public string BackImagePath { get; set; }
+        public string BackAudioPath { get; set; }
+
+        //Thông tin ôn tập (giữ nguyên)
+        public DateTime DueDate { get; set; }
+        public double Interval { get; set; }
+        public double EaseFactor { get; set; }
+
+        //Mỗi card chỉ nằm trong 1 Deck thui
+        public Deck Deck { get; set; }
+
+
+
     }
 }
