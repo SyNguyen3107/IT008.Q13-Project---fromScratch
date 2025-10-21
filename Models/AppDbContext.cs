e34761e42ade88ea7d5f07328f399e534bcbaeea
mjvorfreudej <24521498@gm.uit.edu.vn>
2025-10-21 17:02:02 +0700

    add card

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -13,6 +13,7 @@ namespace IT008.Q13_Project___fromScratch.Models
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // Deck
             modelBuilder.Entity<Deck>(e =>
             {
                 e.ToTable("Decks");
@@ -21,6 +22,33 @@ namespace IT008.Q13_Project___fromScratch.Models
                 e.Property(d => d.Description).HasMaxLength(1000);
             }
             );
+
+            // Card
+            modelBuilder.Entity<Card>(e =>
+            {
+                e.ToTable("Cards");
+                e.Hashkey(c => c.Id);
+
+                e.Property(c => c.FrontText).IsRequired();
+                e.Property(c => c.BackText).IsRequired();
+
+                e.Property(c => FromImagePath).HasMaxLength(512);
+                e.Property(c => FrontAudioPath).HasMaxLength(512);
+                e.Property(c => BackImagePath).HasMaxLength(512);
+                e.Property(c => BackAudioPath).HasMaxLength(512);
+
+                e.Property(c => c.DueDate).IsRequired();
+                e.Property(c => c.Interval).HasPrecision(18, 6);
+                e.Property(c => c.EaseFactor).HasPrecision(18, 6);
+
+                e.HasOne(c => c.Deck)
+                 .WithMany(d => d.Cards)
+                 .HasForeignKey(c => c.DeckId)
+                 .OnDelete(DeleteBehavior.Cascade);
+
+                e.HashIndex(c => new { c.DeckId, c.DueDate });
+            });
+
         }
     }
 }
