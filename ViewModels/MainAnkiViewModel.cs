42b1d58ff916921c6976bea05bf203f741b5c4a2
Sy <Sy@DESKTOP-11DG6QT>
2025-10-29 09:57:35 +0700

    Sửa lại cách button "ImportFile" mở ImportWindow thành command thay vì EventHandler. Thêm gán command tương tự vào File -> Import ở menu trên cùng.

--- ViewModels/MainAnkiViewModel.cs
+++ ViewModels/MainAnkiViewModel.cs
@@ -6,8 +6,11 @@ using IT008.Q13_Project___fromScratch.Services;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
+
+using Microsoft.Win32; // Dùng cho OpenFileDialog
+using System;             // Dùng cho Environment
+using System.Diagnostics; // Dùng cho Debug.WriteLine
+using System.Windows.Input;
 using System.Threading.Tasks;
 
 namespace IT008.Q13_Project___fromScratch.ViewModels
@@ -23,10 +26,10 @@ namespace IT008.Q13_Project___fromScratch.ViewModels
         public MainAnkiViewModel(IDeckRepository deckRepository, INavigationService navigationService)
         {
             _deckRepository = deckRepository;
-            _navigationService = navigationService;//tạo đối tượng navigationService để gọi gián tiếp các View
+            _navigationService = navigationService; //tạo đối tượng navigationService để gọi gián tiếp các View
         }
 
-        [RelayCommand] // Dòng 37 đây, giờ sẽ hết lỗi
+        [RelayCommand]
         private void StartStudy(Deck selectedDeck)
         {
             if (selectedDeck == null) return;
@@ -38,7 +41,31 @@ namespace IT008.Q13_Project___fromScratch.ViewModels
         {
             _navigationService.ShowCreateDeckWindow();
         }
+        [RelayCommand]
+        private void ImportFile()
+        {
+            // Đây là code bạn đã viết trong code-behind
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Import";
+            openFileDialog.Filter = "All supported formats (*.apkg, *.txt, *.zip)|*.apkg;*.txt;*.zip" +
+                                    "|Anki Deck Package (*.apkg)|*.apkg" +
+                                    "|Text file (*.txt)|*.txt" +
+                                    "|Zip file (*.zip)|*.zip";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
+            bool? result = openFileDialog.ShowDialog();
+
+            if (result == true)
+            {
+                string selectedFilePath = openFileDialog.FileName;
+                Debug.WriteLine($"File to import: {selectedFilePath}");
+                // TODO: Gọi service để xử lý file (ví dụ: _importService.Import(selectedFilePath))
+            }
+            else
+            {
+                // Người dùng đã nhấn "Cancel"
+            }
+        }
         public async Task LoadDecksAsync() // async giúp ứng dụng không bị "đơ" khi đang tải dữ liệu từ CSDL.
         {
             var decks = await _deckRepository.GetAllAsync();
--- Views/MainAnkiWindow.xaml.cs
+++ Views/MainAnkiWindow.xaml.cs
@@ -1,19 +1,6 @@
-﻿using IT008.Q13_Project___fromScratch.ViewModels;
-using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.Input;
+using IT008.Q13_Project___fromScratch.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace IT008.Q13_Project___fromScratch
 {
@@ -32,36 +19,7 @@ namespace IT008.Q13_Project___fromScratch
             //Gọi hàm LoadDecksAsync khi cửa sổ được tải
             Loaded += async (s, e) => await _viewModel.LoadDecksAsync();
 
-        }
-
-        private void ImportFileWindowOpen(object sender, RoutedEventArgs e)
-        {
-            //Cửa sổ Import File
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Title = "Import";
-            openFileDialog.Filter = "All supported formats (*.apkg, *.txt, *.zip)|*.apkg;*.txt;*.zip" +
-                                "|Anki Deck Package (*.apkg)|*.apkg" +
-                                "|Text file (*.txt)|*.txt" +
-                                "|Zip file (*.zip)|*.zip";
-            //Thư mục mở mặc định, tạm thời để là My Document
-            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-
-            bool? result = openFileDialog.ShowDialog();
-
-            if (result == true)
-            {
-                //Lấy đường dẫn file người dùng đã chọn
-                string selectedFilePath = openFileDialog.FileName;
-
-                // TODO: Gọi service hoặc phương thức để xử lý file này
-                Debug.WriteLine($"File to import: {selectedFilePath}");
-            }
-            else
-            {
-                // Người dùng đã nhấn "Cancel"
-            }
-        }
-        
+        }      
         private async void MainAnkiWindowLoaded(object sender, RoutedEventArgs e)
         {
             // 1. Lấy "bộ não" (ViewModel) từ DataContext
