cf482f0392386c0f42220769be50a3974f43ca51
NguyenPhamThuyVy <24522059@gm.uit.edu.vn>
2025-12-22 21:17:29 +0700

    MemberGameWindow.xaml

---
+++ ViewModels/MemberGameViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows;
+
+namespace EasyFlips.ViewModels
+{
+    public partial class MemberGameViewModel : ObservableObject
+    {
+        // Binding IsInputEnable
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(SubmitAnswerCommand))] // Tự cập nhật trạng thái nút bấm
+        private bool isInputEnabled;
+
+        [ObservableProperty]
+        private string userAnswer;
+
+        public MemberGameViewModel()
+        {
+            // Mặc định là KHÓA khi mới vào (chưa có câu hỏi)
+            IsInputEnabled = false;
+        }
+        // Gọi hàm này khi nhận tín hiệu "Question" từ Server/Host
+        public void OnQuestionReceived()
+        {
+            UserAnswer = "";        // Xóa đáp án cũ
+            IsInputEnabled = true;  // MỞ KHÓA: Cho phép nhập và nhấn Enter
+        }
+        // Hàm xử lý nộp bài(Dùng cho cả Nút Submit và Phím Enter)
+        [RelayCommand(CanExecute = nameof(CanSubmit))]
+        private void SubmitAnswer()
+        {
+            // Xử lý logic nộp bài ở đây
+            MessageBox.Show($"Đã nộp đáp án: {UserAnswer}");
+
+            // Sau khi nộp xong thì KHÓA lại ngay
+            IsInputEnabled = false;
+        }
+        // Điều kiện để được phép nộp (Chỉ nộp được khi IsInputEnabled = true)
+        private bool CanSubmit()
+        {
+            return IsInputEnabled;
+        }
+    }
+}
---
+++ Views/MemberGameWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using EasyFlips.ViewModels;
+
+namespace EasyFlips.Views
+{
+    /// <summary>
+    /// Interaction logic for MemberGameWindow.xaml
+    /// </summary>
+    public partial class MemberGameWindow : Window
+    {
+        public MemberGameWindow()
+        {
+            InitializeComponent();
+            // Gắn "Bộ não" (ViewModel) vào "Giao diện" (Window)
+            this.DataContext = new MemberGameViewModel();
+        }
+    }
+}
